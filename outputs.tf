c7818fc1e385bd4efe7493630a2cffff9b11c5da
Matt Palmer <mpalmer@hezmatt.org>
2021-10-18 21:58:05 +1100

    Add dynamodb_table_arn output
    Useful for passing the table ARN to other parts of the infrastructure,
    if you're into that sort of thing.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "lambda_role_arn" {
   description = "The ARN of the IAM role which the issue_cert Lambda assumes"
   value       = aws_iam_role.issue_cert.arn
 }
+
+output "dynamodb_table_arn" {
+  description = "The ARN of the DynamoDB table name that stores the L'ACME certificate/key/chain"
+  value       = aws_dynamodb_table.lacme.arn
+}
